1eab4deb7d13c1bda1b799b9a0150a86ada19a8a
Abdulrahman AlKhaldi <alkhaldirahman@gmail.com>
2024-10-29 20:02:41 +0300

    Add HomeView

---
+++ SwiftSourceControl/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  SwiftSourceControl
+//
+//  Created by Abdulrahman AlKhaldi on 29/10/2024.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Screen 2!")
+    }
+}
+
+#Preview {
+    HomeView()
+}
